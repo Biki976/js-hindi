c85f8389216351b2d4b6e1daf1ff9d600daf52f2
Biki976 <bikichangmai21@gmail.com>
2024-10-06 07:26:34 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "bikichangmai21@gmail.com"
+var accountPassword = " 12345"
+accountCity = "Assam"
+
+// accountId = 2
+
+
+console.log(accountId);
+console.table([accountId, accountEmail,accountPassword,accountCity])
